994f644ee9804d9c3e9e4e0cced7115e04a1568a
Levi Lima <leviimediata@hotmail.com>
2024-04-10 14:18:24 -0300

    Add files via upload
    depois tem que olhar o que tá repetido

--- Agente.java
+++ Agente.java
@@ -1,5 +1,74 @@
-package AgenteSecretoPOO;
 
 public class Agente {
+
+    private String nome;
+    private String sigla;
+    private String prefixoArea;
+    private Enum.NivelHierarquico nivelHierarquico;
+    private Pais pais;
+    private String informacoes;
+    private String pseudonimo;
+
+    public Agente() {}
+
+    public Agente(String nome, String sigla, String prefixoArea, Enum.NivelHierarquico nivelHierarquico, Pais pais) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.prefixoArea = prefixoArea;
+        this.nivelHierarquico = nivelHierarquico;
+        this.pais = pais;
+    }
+
+    // Getters e Setters
+    public String getNome() { 
+        return nome; 
+    }
+    public void setNome(String nome) { 
+        this.nome = nome; 
+    }
+    public String getSigla() {
+        return sigla; 
+    }
+    public void setSigla(String sigla) { 
+        this.sigla = sigla; 
+    }
+    public String getPrefixoArea() { 
+        return prefixoArea; 
+    }
+    public void setPrefixoArea(String prefixoArea) { 
+        this.prefixoArea = prefixoArea;
+    }
+    public Enum.NivelHierarquico getNivelHierarquico() {
+        return nivelHierarquico; 
+    }
+    public void setNivelHierarquico(Enum.NivelHierarquico nivelHierarquico) { 
+        this.nivelHierarquico = nivelHierarquico; 
+    }
+    public Pais getPais() { 
+        return pais; 
+    }
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+    public String getInformacoes() { 
+        return informacoes; 
+    }
+    public void setInformacoes(String informacoes) { 
+        this.informacoes = informacoes; 
+    }
+    public String getPseudonimo() {
+        return pseudonimo; 
+    }
+    public void setPseudonimo(String pseudonimo) { 
+        this.pseudonimo = pseudonimo;
+    }
+
+    
+    public void enviarMensagem(Mensagem mensagem) { 
+       // postMensagem();
+    }
+    public void receberMensagem(Mensagem mensagem) { 
+        //getMmensagem();    
+    }
 
 }
---
+++ Enum.java
@@ -0,0 +1,15 @@
+public class Enum {
+    public enum NivelHierarquico {
+        JUNIOR,
+        PLENO,
+        SENIOR,
+        COMANDO
+    }
+
+    public enum NivelSeguranca {
+        BAIXO,
+        MEDIO,
+        ALTO,
+        TOPSECRET
+    }
+}
