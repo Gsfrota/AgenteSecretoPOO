1ed7e9ef591ec2a29b309ca99c812fab6ef5c334
levilimas <leviimediata@hotmail.com.com>
2024-04-10 15:22:03 -0300

    test

--- AgenciaInteligencia.java
+++ AgenciaInteligencia.java
@@ -1,52 +1,38 @@
 package AgenteSecretoPOO;
 
-public class AgenciaInteligencia{
+public class AgenciaInteligencia {
     private String nome;
-    private String chave;
-    private String alfabeto;
-    private String pais;
+    private Pais pais;
 
-    AgenciaInteligencia(String nome, String chave, String alfabeto){
-        this.nome = nome;
-        this.chave = chave;
-        this.alfabeto = alfabeto;
-        this.pais = pais;
+    public AgenciaInteligencia() {
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getNome(){
+
+    public String getNome() {
         return nome;
     }
 
-    public void setChave(String chave){
+    public void setChave(String chave) {
         this.chave = chave;
     }
-    public String getChave(){
-        return chave;
-    }
 
-    public void setAlfabeto(String alfabeto){
-        this.alfabeto = alfabeto;
-    }
-    public String getAlfabeto(){
-        return alfabeto;
-    }
-
-    public void setPais(String pais){
+    public void setPais(Pais pais) {
         this.pais = pais;
     }
-    public String getPais(){
+
+    public Pais getPais() {
         return pais;
     }
 
     public String decifrarMensagem(String mensagem, Agente agente) {
         return " ";
     }
-       
-        public String cifrarMensagem(String mensagem, AgenciaInteligencia agencia) {
-          
-            return ""; 
-        }
+
+    public String cifrarMensagem(String mensagem, AgenciaInteligencia agencia) {
+
+        return "";
+    }
 }
